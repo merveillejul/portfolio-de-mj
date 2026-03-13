866c068b565a0d21d893c82c2052848b7f3343c3
merveillejul <merveillenourryssou@outlook.fr>
2026-03-13 16:51:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 window.addEventListener("scroll", function(){
-        const navbar = document.querySelector(".navbar");
-        if(window.scrollY > 50){
-            navbar.classList.add("scrolled");
-        } else {
-            navbar.classList.remove("scrolled");
-        }
-    });
 
+const navbar = document.querySelector(".navbar");
 
+if(!navbar) return;
+
+if(window.scrollY > 50){
+navbar.classList.add("scrolled");
+}
+else{
+navbar.classList.remove("scrolled");
+}
+
+});
     
 
 const burger = document.getElementById("burger");
